03debd815b4db4f258f8e5f1209c45193a7f7441
Paul <pc.paul.leung@gmail.com>
2017-08-23 14:51:40 +0100

    Polymer 2.0 Hybrid
    Mostly functioning. Tested on Chrome, Edge, Firefox

--- bower_components/app-storage/app-indexeddb-mirror/common-worker-scope.js
+++ bower_components/app-storage/app-indexeddb-mirror/common-worker-scope.js
@@ -36,7 +36,7 @@
   self.addEventListener('message', function(event) {
     var data = event.data;
 
-    if (data && data.type === 'common-worker-connect') {
+    if (data && data['type'] === 'common-worker-connect') {
       var EventConstructor =
           self.CustomEvent ||
           self.Event ||
@@ -50,5 +50,5 @@
     }
   }.bind(this));
 
-  self.importScripts([workerScript]);
+  self.importScripts(workerScript);
 })();
